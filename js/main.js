c918e3ebf59e30e58f51f3c958c8964ae4dc2bd6
Gon <gon27.tk08@gmail.com>
2020-04-03 22:45:27 +0900

    画面のサイズを変更しても、ボックスがきれいに並ぶようにした

--- js/main.js
+++ js/main.js
@@ -7,8 +7,18 @@ function main(){
     let workBox = document.getElementsByClassName("work-box");
     console.log(workBox);
 
+    let offsetWidth = workBox[0].offsetWidth;
+    let workContainerWidth = workContainer[0].offsetWidth;
+    // console.log(offsetWidth)
+    // console.log(workContainer[0].offsetWidth)
+    // console.log(Math.floor(workContainer[0].offsetWidth / offsetWidth))
+
+    let limit = Math.floor(workContainerWidth / offsetWidth);
+
+    // console.log(limit)
+
     let length = workBox.length;
-    let times = (3 * Math.ceil(length / 3)) - length;
+    let times = (limit * Math.ceil(length / limit)) - length;
 
     for(let i = 0; i < times; i++){
         let div = document.createElement('div');
